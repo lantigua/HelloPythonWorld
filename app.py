bcebc18f6c905a7e4ed0f8a984fba50618d0a85d
Pedro L. Lantigua <lantigua.pl@gmail.com>
2019-09-17 17:18:03 -0400

    new python code

---
+++ app.py
@@ -0,0 +1,15 @@
+info = 'Python is an interpreted, high-level, general-purpose programming language.'
+print(info)
+print(info[-1])
+print(info[0:6])
+print(info[-9:-1])
+
+birth_year = input("Birth year: ")
+print(type(birth_year))
+age = 2019 - int(birth_year)
+print(age)
+print(type(age))
+
+lbs_weight = input("Weight (Lbs: " )
+kg_weight = 0.45 * int(lbs_weight)
+print(kg_weight)
