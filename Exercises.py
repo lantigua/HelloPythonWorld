453dc7b3551b65962e2478c1eafd34e4fb043641
Admin <lantigua.pl@gmail.com>
2019-09-23 23:36:25 -0400

    new code

--- Exercises.py
+++ Exercises.py
@@ -9,8 +9,36 @@ print(str.format(cuc_total))
 
 #--------------------------------------------------
 
+uniques_numbers = []
+for numbers in number_list1:
+    if numbers not in uniques_numbers:
+        uniques_numbers.append(numbers)
+print(uniques_numbers)
+uniques_numbers.sort()
+print(uniques_numbers)
+
+#--------------------------------------------------
+
 for x_s in number_list1:
     output = ""
     for count in range(x_s):
         output += "*"
     print(output)
+
+#---------------------------------------------------
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+for filas in matrix:
+    for cosas in filas:
+        print("cosas de las filas {}".format(cosas))
+#---------------------------------------------------
+
+large = number_list1[0]
+for item in number_list1:
+    if item > large:
+        large = item
+print("The largest number in the list is {}".format(large))
---
+++ more.py
@@ -0,0 +1,12 @@
+# unpacking tuples
+coordinates = (6, 2, 7)
+x, y, z = coordinates
+
+# dictionaries
+customer = {
+    "name": "Pedro Luis",
+    "age": 33,
+    "lenguage": "spanish",
+    "is_verified": True
+}
+print(customer["name"])
